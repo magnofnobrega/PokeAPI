1f25fa68570b526cb7fa9bd6209ca1b6eb998ec6
Magno Furtado Nóbrega <magnofnobrega@outlook.com>
2025-06-25 12:53:47 -0300

    feat: adiciona navegação com nome, habilidades e status + ajustes visuais

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,6 +14,10 @@ import { CommonModule } from '@angular/common';
 export class HomePage implements OnInit {
   pokemon: any = {};
 
+  paginas: { label: string, value: string }[] = [];
+  paginaAtual: { label: string, value: string } | null = null;
+  indicePaginaAtual: number = 0;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -23,10 +27,88 @@ export class HomePage implements OnInit {
   getPokemon(name: string) {
     const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
     this.http.get(url).subscribe((data: any) => {
+      const tipos = data.types.map((t:any) => t.type.name);
+
       this.pokemon = {
         name: data.name,
         image: data.sprites.front_default,
+        tipo1: this.traduzirTipo(tipos[0] || ''),
+        tipo2: this.traduzirTipo(tipos[1] || '')
       };
+
+      const traduzirStatus = (nome: string): string => {
+        const mapa: { [key: string]: string } = {
+          hp: 'HP',
+          attack: 'Ataque',
+          defense: 'Defesa',
+          'special-attack': 'Esp.Ataque',
+          'special-defense': 'Esp.Defesa',
+          speed: 'Velocidade'
+        };
+        return mapa[nome] || nome;
+      };
+
+      this.paginas = [
+        {label: 'Nome', value: data.name },
+        {label: 'Altura', value: `${data.height / 10}m` },
+        {label: 'Peso', value: `${data.weight / 10}kg` },
+        {label: 'Habilidades', value: data.abilities.map((a: any) => a.ability.name).join(', ') },
+        {label: 'Status', 
+          value: data.stats.map((s: any) => ({
+          nome: traduzirStatus(s.stat.name), 
+          valor: s.base_stat
+          }))
+        }
+      ];
+
+      this.definirPagina(0);
+
     });
+  }
+
+  definirPagina(indice: number) {
+    if (indice >= 0 && indice <= 2) {
+      this.indicePaginaAtual = indice;
+      this.paginaAtual = this.paginas[indice];
+    }
+  }
+
+  proxima() {
+    if (this.indicePaginaAtual < 2)
+      this.definirPagina(this.indicePaginaAtual + 1);
+  }
+
+  anterior() {
+    if (this.indicePaginaAtual > 0) {
+      this.definirPagina(this.indicePaginaAtual - 1);
+    }
+  }
+
+  traduzirTipo(tipo: string): string {
+  const traducoes: { [key: string]: string } = {
+    normal: 'Normal',
+    fire: 'Fogo',
+    water: 'Água',
+    electric: 'Elétrico',
+    grass: 'Grama',
+    ice: 'Gelo',
+    fighting: 'Lutador',
+    poison: 'Veneno',
+    ground: 'Terra',
+    flying: 'Voador',
+    psychic: 'Psíquico',
+    bug: 'Inseto',
+    rock: 'Pedra',
+    ghost: 'Fantasma',
+    dragon: 'Dragão',
+    dark: 'Sombrio',
+    steel: 'Aço',
+    fairy: 'Fada',
+    };
+    return traducoes[tipo] || tipo;
+  }
+
+  isStatusArray(value: any): value is { nome: string; valor: number }[] {
+    return Array.isArray(value);
   }
 }
